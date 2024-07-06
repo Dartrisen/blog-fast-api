e955e8fd97e1370326973e1b8670b0ec039d44eb
dartrisen <calculating.scientist@gmail.com>
2024-07-06 18:08:16 +0200

    models have been added

---
+++ models.py
@@ -0,0 +1,46 @@
+from sqlalchemy import Column, Integer, String, Text, ForeignKey, DateTime, Boolean
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime, timezone
+from database import Base
+
+
+class EntityBase:
+    id = Column(Integer, primary_key=True, index=True)
+    created_at = Column(DateTime, default=datetime.now(timezone.utc))
+
+
+class User(Base, EntityBase):
+    __tablename__ = "users"
+
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True)
+    is_superuser = Column(Boolean, default=False)
+
+    posts = relationship("Post", back_populates="owner")
+    comments = relationship("Comment", back_populates="author")
+
+
+class Post(Base, EntityBase):
+    __tablename__ = "posts"
+
+    title = Column(String, index=True, nullable=False)
+    content = Column(Text, nullable=False)
+    published = Column(Boolean, default=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="posts")
+    comments = relationship("Comment", back_populates="post")
+
+
+class Comment(Base, EntityBase):
+    __tablename__ = 'comments'
+
+    content = Column(Text, nullable=False)
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    author_id = Column(Integer, ForeignKey('users.id'))
+
+    post = relationship("Post", back_populates="comments")
+    author = relationship("User", back_populates="comments")
