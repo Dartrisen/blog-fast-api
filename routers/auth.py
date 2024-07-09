b541a9521fd8def6e9d3d54ad69cbd2c6095c315
dartrisen <calculating.scientist@gmail.com>
2024-07-09 10:26:38 +0200

    auth.py have been added

---
+++ routers/auth.py
@@ -0,0 +1,23 @@
+from datetime import datetime, timedelta, timezone
+from typing import Annotated, Type
+
+import bcrypt
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from jose import jwt, JWTError
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from starlette import status
+
+from ..database import SessionLocal
+from models import User
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["auth"]
+)
+
+SECRET_KEY = "557194d75e3f6ac06f08816bc59f0f62e1093542bb9104d229baeda94e7c6ba7"
+ALGORITHM = "HS256"
+
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/token")
