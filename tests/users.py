17cc40a9471a41f3235e0e83941002817302659a
dartrisen <calculating.scientist@gmail.com>
2024-08-09 01:00:14 +0200

    user tests

---
+++ tests/users.py
@@ -0,0 +1,44 @@
+from starlette import status
+
+from routers.users import get_db, get_current_user
+from .utils import *
+
+app.dependency_overrides[get_db] = override_get_db
+app.dependency_overrides[get_current_user] = override_get_current_user
+
+
+def test_return_user(test_user):
+    response = client.get("/user")
+    assert response.status_code == status.HTTP_200_OK
+    result = response.json()
+    result.pop("hashed_password")
+    result.pop("created_at")
+    assert result == {
+        'email': 'test@test.com',
+        'id': 1,
+        'is_active': True,
+        'is_superuser': True,
+        'username': 'dartrisen',
+    }
+
+
+def test_change_password_success(test_user):
+    response = client.put("/user/password", json={
+        "password": "testpassword",
+        "new_password": "newpassword"
+    })
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+
+def test_change_password_invalid_current_password(test_user):
+    response = client.put("/user/password", json={
+        "password": "wrongpassword",
+        "new_password": "newpassword"
+    })
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json() == {"detail": "Error on password change"}
+
+
+def test_change_email_success(test_user):
+    response = client.put("/user/email/test@test.com")
+    assert response.status_code == status.HTTP_204_NO_CONTENT
