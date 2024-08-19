f6a505069558b601769ca11692f0895b2efc57cb
dartrisen <calculating.scientist@gmail.com>
2024-08-19 09:04:16 +0200

    post.py tests

--- tests/posts.py
+++ tests/posts.py
@@ -17,3 +17,15 @@ def test_get_posts(test_post):
         "owner_id": 1,
         "published": True,
     }]
+
+
+def test_get_post(test_post):
+    response = client.get("/posts/1")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {
+        "content": "I need to lock in...",
+        "title": "Test Title",
+        "id": 1,
+        "owner_id": 1,
+        "published": True,
+    }
